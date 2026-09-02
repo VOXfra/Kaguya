f244de961db837c5861f2a95a907a2daac67031d
VOXfra <shift.partenariat@gmail.com>
2026-09-02 04:33:27 +0200

    Fix compact return tokens in content probe build

---
+++ tools/rdr2-content-probe/msvc_token_fix.h
@@ -0,0 +1,11 @@
+#pragma once
+#define return100 return 100
+#define return96 return 96
+#define return90 return 90
+#define return88 return 88
+#define return82 return 82
+#define return78 return 78
+#define return75 return 75
+#define return55 return 55
+#define return25 return 25
+#define return40 return 40
