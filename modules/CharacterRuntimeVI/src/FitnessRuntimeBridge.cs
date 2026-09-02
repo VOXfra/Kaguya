709724e455797cbe87a0a22eeadd10fce88be911
VOXfra <shift.partenariat@gmail.com>
2026-09-02 02:00:53 +0200

    Publish current fitness profile for visible muscle morph

--- modules/CharacterRuntimeVI/src/FitnessRuntimeBridge.cs
+++ modules/CharacterRuntimeVI/src/FitnessRuntimeBridge.cs
@@ -5,6 +5,26 @@ namespace VOX.CharacterRuntimeVI
     internal static class FitnessRuntimeBridge
     {
         public static Action<float, float, float> AddTraining;
+        public static float CurrentStrength;
+        public static float CurrentEndurance;
+        public static float CurrentLeanMass;
+        public static bool CurrentProfileValid;
+
+        public static void Publish(float strength, float endurance, float leanMass)
+        {
+            CurrentStrength = strength;
+            CurrentEndurance = endurance;
+            CurrentLeanMass = leanMass;
+            CurrentProfileValid = true;
+        }
+
+        public static void ClearPublished()
+        {
+            CurrentStrength = 0f;
+            CurrentEndurance = 0f;
+            CurrentLeanMass = 0f;
+            CurrentProfileValid = false;
+        }
 
         public static void Train(float strength, float endurance, float leanMass)
         {
